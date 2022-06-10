4e77258369713acfd6fada82b3d1ffa86f466922
Futsum Estifanos <fozyle@yahoo.de>
2022-06-10 23:10:35 +0200

    first commit of rate_my_app

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:launch_review/launch_review.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text("Test app"),
+        actions: [
+          Padding(padding: EdgeInsets.all(10),
+            child: IconButton(
+              icon: Icon(Icons.rate_review_outlined),
+              onPressed: (){
+                LaunchReview.launch(
+                 androidAppId: "com.example.testapp",iOSAppId: "44444457"
+                );
+              },
+            ),
+          )
+        ],
+      ),
+      body:  Center(
+        child: Text("Rating app"),
+      ),
+    );
+  }
+}
